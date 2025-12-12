e4d81befda1a3676241e9ee13c3e2a2188b22b6a
Vlad11ab <breazuvlad11@gmail.com>
2025-12-12 20:52:49 +0200

    querying_relational_databases

--- Query.sql
+++ Query.sql
@@ -103,6 +103,74 @@ select max(age) from profesor;
 
 
 
+create table book (
+    id integer unique not null primary key,
+    studentId integer not null,
+    bookName varchar(255) null,
+
+    foreign key (studentId) references student(id)
+);
+insert into book (id, bookName, studentId)
+values  (1, 'clean code',  1),
+        (2, 'spring boot',  1),
+        (3, 'think like a programmer',  2);
+insert into book (id, bookName, studentId) values
+(4, 'effective java', 3),
+(5, 'java concurrency', 3),
+(6, 'introduction to algorithms', 5),
+(7, 'postgresql up and running', 6);
+
+
+create table student_id_card(
+    id integer unique not null primary key,
+    studentId integer not null,
+    cardNumber integer unique not null,
+
+    foreign key (studentId) references student(id)
+);
+insert into student_id_card (id, cardNumber, studentId)
+values      (1, '1234567890', 1),
+            (2, '0987654321', 2);
+insert into student_id_card (id, cardNumber, studentId) values
+(3, '1000000003', 3),
+(4, '1000000004', 4),
+(5, '1000000005', 5),
+(6, '1000000006', 6);
+
+
+select s.firstName, s.lastName, c.cardNumber
+from student as s
+inner join student_id_card as c on s.id = c.studentId;
+
+select s.firstName, s.lastName, c.name as materie
+from student as s
+inner join enrollment as e on e.studentId = s.id
+inner join curs as c on c.id = e.cursId;
+
+select s.firstName, s.lastName, b.bookName
+from student as s
+left outer join book as b on b.studentId = s.id;
+
+select s.firstName, s.lastName, c.name
+from student as s
+join enrollment as e on e.studentId = s.id
+right outer join curs as c on c.id = e.cursId;
+
+select email from student
+union
+select email from profesor
+union
+select email from user;
+
+select lastName from student
+union all
+select lastName from profesor;
+
+select email from user
+intersect
+select email from student;
+
+
 
 
 
